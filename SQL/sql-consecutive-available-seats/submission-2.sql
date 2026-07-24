9a832a3979afb88d83fbcf69c9127f24feaf2594
Faisal Junaid Bukhari Syed <84629988+faisyed@users.noreply.github.com>
2026-07-24 14:53:31 -0400

    Add: sql-consecutive-available-seats - submission-2

---
+++ SQL/sql-consecutive-available-seats/submission-2.sql
@@ -0,0 +1,4 @@
+-- Write your query below
+select distinct c1.seat_id
+from cinema c1 join cinema c2 on (c1.seat_id = c2.seat_id + 1 or c1.seat_id = c2.seat_id - 1) and c1.free=1 and c2.free=1
+order by c1.seat_id;
