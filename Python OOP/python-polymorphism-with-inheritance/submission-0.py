c6196ff62abaa11ba8d7a0ba6893b47e97e0dd61
Faisal Junaid Bukhari Syed <84629988+faisyed@users.noreply.github.com>
2026-08-13 21:45:45 -0400

    Add: python-polymorphism-with-inheritance - submission-0

---
+++ Python OOP/python-polymorphism-with-inheritance/submission-0.py
@@ -0,0 +1,27 @@
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def make_sound(self) -> None:
+        print("Animal is making a sound")
+
+class Dog(Animal):
+    def make_sound(self):
+        print(f"{self.name} says: Woof!")
+
+class Cat(Animal):
+    def make_sound(self):
+        print(f"{self.name} says: Meow!")
+
+def helper(ani: Animal):
+    ani.make_sound()
+
+# Do not change the code below
+animal = Animal("Rabbit")
+animal.make_sound()
+
+animal = Dog("Buddy")
+animal.make_sound()
+
+animal = Cat("Whiskers")
+animal.make_sound()
